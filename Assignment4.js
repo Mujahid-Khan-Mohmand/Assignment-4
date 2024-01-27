443de43d0c82c021d34ada1ff1b1092409258c12
Mujahid-Khan-Mohmand <155892674+Mujahid-Khan-Mohmand@users.noreply.github.com>
2024-01-27 22:06:10 +0500

    Add files via upload

---
+++ Assignment4.js
@@ -0,0 +1,27 @@
+let num = prompt("Enter the Number: ");
+let number = Number(num);
+
+
+for(let i=11;i<=20;i++)
+{
+    console.log(number+" "+  "*"+" " +i +" "  +'='+"  "  +number*i);
+}
+
+
+
+let a = prompt("Enter the Any Number: ");
+let nm = Number(a);
+
+let factorial = 1;
+let i = 1;
+while(i<=nm)
+{
+    factorial = factorial*i;
+    i++;
+}
+    console.log("Facotrial: "+factorial);
+
+
+
+
+
